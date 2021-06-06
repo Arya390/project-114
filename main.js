f5cc2c80d5b9f0f82679640079d3f8fb4846ee21
Arya390 <75195330+Arya390@users.noreply.github.com>
2021-06-06 17:48:25 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,7 @@
+noseX=0;
+noseY=0;
 function preload() {
+    mustache = loadImage('https://i.postimg.cc/3x3QzSGq/m.png');
 }
 
 function setup() {
@@ -21,13 +24,16 @@ function gotPoses(results)
     if(results.length > 0)
     {
         console.log(results);
-        console.log("mustache x =" + results[0].pose.mustache.x);
-        console.log("mustache y =" + results[0].pose.mustache.y);
+        noseX = results[0].pose.nose.x - 20;
+        noseY = results[0].pose.nose.y;
+        console.log("nose x = " + noseX);
+        console.log("nose y = " + noseY);
     }
 }
 
 function draw() {
     image(video, 0, 0, 300, 300);
+    image(mustache, noseX, noseY, 40, 40);
 }
 
 function take_snapshot() 
